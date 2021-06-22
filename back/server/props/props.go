32eed0a0f2de6b0d8e92c640fe36ade08acf4444
54m <30588003+54m@users.noreply.github.com>
2021-06-22 23:09:23 +0900

    update(back): add config

--- back/server/props/props.go
+++ back/server/props/props.go
@@ -1,7 +1,12 @@
 // Package props is a scaffold file for props of controllers
 package props
 
+import (
+	"github.com/star-integrations/project-boilerplate/back/pkg/config"
+)
+
 // ControllerProps is passed from Bootstrap() to all controllers
 type ControllerProps struct {
 	// DB, config, etc...
+	Config *config.Config
 }
