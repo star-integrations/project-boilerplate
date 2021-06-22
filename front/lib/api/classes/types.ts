4e272c7137522e9b0cd8c9705fa8082cf73536d1
54m <30588003+54m@users.noreply.github.com>
2021-06-22 23:11:02 +0900

    add(front): generated code

---
+++ front/lib/api/classes/types.ts
@@ -0,0 +1,8 @@
+// THIS FILE IS A GENERATED CODE.
+// DO NOT EDIT THIS CODE BY YOUR OWN HANDS
+// generated version: 2.1.0
+
+export type GetHealthCheckRequest = {}
+export type GetHealthCheckResponse = {
+	status: number;
+}
