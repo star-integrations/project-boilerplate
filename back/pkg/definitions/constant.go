1b6914361aa25b02d92a8dd67189a7ff89a3556d
54m <30588003+54m@users.noreply.github.com>
2021-10-12 22:51:29 +0900

    feat: add definitions pkg

---
+++ back/pkg/definitions/constant.go
@@ -0,0 +1,10 @@
+package definitions
+
+const (
+	EnvKeyGoogleCloudProject = "GOOGLE_CLOUD_PROJECT" // for Appengine
+	EnvKeyProjectID          = "GCP_PROJECT"          // for Google Cloud Functions
+	EnvKeyServiceName        = "GAE_SERVICE"
+	EnvKeyServiceVersion     = "GAE_VERSION"
+	StorageHost              = "https://storage.googleapis.com/"
+	DefaultPagingLimit       = 11
+)
