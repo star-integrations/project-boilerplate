a1dac3c18889cb11fb67806d0b53b3d95e4f45e9
54m <30588003+54m@users.noreply.github.com>
2021-10-12 22:57:58 +0900

    fix: scripts

--- scripts/api_gen-checker.sh
+++ scripts/api_gen-checker.sh
@@ -1,7 +1,7 @@
-make generate
+make server_generate client_generate
 clean=$(git status | grep "nothing to commit" || true)
 if [ -z "$clean" ]; then
   git status
-  echo "生成コードが一致しません。make generateを実行してください。"
+  echo "生成コードが一致しません。make server_generate client_generateを実行してください。"
   exit 1
 fi
